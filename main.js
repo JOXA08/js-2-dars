2122adc70f581f51c26d482bed9f3b877bc78b67
Jahongir Safarov <168184861+JOXA08@users.noreply.github.com>
2024-05-06 17:23:00 +0500

    js 2 dars
    zeromax itcentr

---
+++ main.js
@@ -0,0 +1,78 @@
+/* if =>    Agar */
+/* else =>  u holda */
+/* else if  yoki bo'lmasa */
+
+/*if(45 <= 44){
+    console.log("Solishtrayotgan son 45ga teng yoki katta")
+}else{
+    console.log("Solishtirilayotgan son 45ga teng emas yoki katta")
+}*/
+
+/*if (45 == 46){
+    console.log("46 45ga teng emas")
+}else if (45 == 44){
+    console.log("44 45ga teng emas")
+}else{
+    console.log("yuqoridagi shartlardan birortasi bajarilmadi")
+}*/
+
+/*
+if (12.00 == 1.00){
+    console.log("12:00 1:00 ga teng emas")
+}else if (11.00 == 11.00){
+    console.log("11:00 11:00ga teng")
+}else{
+    console.log("yuqoridagi shartlardan birortasi bajarilmadi")
+}
+
+if(12.00 <= 1.00){
+    console.log("Solishtrayotgan son 12:00ga teng yoki katta")
+}else{
+    console.log("Solishtirilayotgan son 12:00ga teng emas yoki katta")
+}
+
+let a =45;
+
+if (a >= 46){
+    console.log("a berilgan sondan kichik yoki teng")
+}else{
+    console.log("a ning qiymati topilmadi")
+}*/
+
+let a = 46
+let b = 10
+let c = 19
+let d = 49
+
+/*if (a == 46 && b == 10 && c == 19 && d == 49){
+    console.log("shartlarning hammasi togri")
+}
+else{
+    console.log("shartlarning hammasi yoki kamida bittasi notogri")
+}*/
+
+/*if (a == 46 || b == 10 || c == 19 || d == 49){
+    console.log("shartlarning kamida 1 tasi yoki hammasi togri")
+}
+else{
+    console.log("shartlarning hammasi notogri")
+}*/
+
+let a = 46
+let b = 10
+let c = 19
+let d = 49
+
+if (a == 46 || b == 10 || c == 19 || d == 49){
+    console.log("shartlarning kamida 1 tasi yoki hammasi togri")
+}
+else{
+    console.log("shartlarning hammasi notogri")
+}
+if (a == 46 && b == 10 && c == 19 && d == 49){
+    console.log("shartlarning hammasi togri")
+}
+else{
+    console.log("shartlarning hammasi yoki kamida bittasi notogri")
+}else if (a == 46 || b == 10 || c == 19 || d == 49){
+    console.log("11:00 11:00ga teng")}
